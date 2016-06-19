15c8377e4a8f268f7f9de1f85047dca1b186316c
Francesco Commisso <Francesco_Commisso@hotmail.com>
2016-06-19 12:47:13 -0400

    test

--- app/src/main/java/com/francescocommisso/sitemanager/DBHandler.java
+++ app/src/main/java/com/francescocommisso/sitemanager/DBHandler.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 
 public class DBHandler extends SQLiteOpenHelper {
 
+    //ass
     private static DBHandler instance = null;
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "sites.db";
